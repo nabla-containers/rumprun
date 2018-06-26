07c0f44f39653eff3632e5b4a6d97a0ef85412b1
Ricardo Koller <kollerr@us.ibm.com>
2018-06-26 15:52:11 -0400

    Adding mount rootfs test

---
+++ rootfs_mount_test/rootfs.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <dirent.h>
+
+int list_dirs(char *path)
+{
+    struct dirent *de;
+    DIR *dr = opendir(path);
+ 
+    if (dr == NULL)  // opendir returns NULL if couldn't open directory
+    {
+        printf("Could not open directory" );
+        return 0;
+    }
+ 
+    while ((de = readdir(dr)) != NULL)
+            printf("%s\n", de->d_name);
+ 
+    closedir(dr);    
+    return 0;
+}
+
+int main(int argc , char *argv[])
+{
+	int i = 1;
+	int n;
+	char buf[80];
+
+	char cwd[1024];
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		fprintf(stdout, "Current working dir: %s\n", cwd);
+	else
+		perror("getcwd() error");
+
+	list_dirs("/");
+
+	int f = open("/tmp/bla", O_CREAT|O_RDWR);
+	assert(f != -1);
+	write(f, "123\n", 4);
+	lseek(f, 0, SEEK_SET);
+	n = read(f, buf, 4);
+	assert(n == 4);
+	write(1, buf, 4);
+	close(f);
+
+	list_dirs("/tmp");
+
+	return 0;
+}
