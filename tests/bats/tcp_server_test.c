bdd4c2438afdd474461aeaecba5950605dc0870b
Ricardo Koller <kollerr@us.ibm.com>
2019-02-12 13:43:32 -0500

    Check ret vals in tcp_server_test.c

--- tests/bats/tcp_server_test.c
+++ tests/bats/tcp_server_test.c
@@ -7,13 +7,17 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
+#include <err.h>
 
 int main(int argc, char *argv[])
 {
 	int listenfd = 0, connfd = 0;
-	struct sockaddr_in serv_addr; 
+	struct sockaddr_in serv_addr;
+	int ret;
 
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenfd == -1)
+		err(1, "socket() failed");
 	memset(&serv_addr, '0', sizeof(serv_addr));
 
 	serv_addr.sin_family = AF_INET;
@@ -21,15 +25,25 @@ int main(int argc, char *argv[])
 	serv_addr.sin_port = htons(5000); 
 
 	int optval = 1;
-	setsockopt(listenfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+	ret = setsockopt(listenfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+	if (ret == -1)
+		err(1, "setsockopt() failed");
 
-	bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+	ret = bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	if (ret == -1)
+		err(1, "bind() failed");
 
-	listen(listenfd, 10); 
+	ret = listen(listenfd, 10);
+	if (ret == -1)
+		err(1, "listen() failed");
 
 	connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+	if (connfd == -1)
+		err(1, "accetp() failed");
 
-	write(connfd, "nabla\n", strlen("nabla\n")); 
+	ret = write(connfd, "nabla\n", strlen("nabla\n"));
+	if (ret != strlen("nabla\n"))
+		err(1, "write() failed");
 
 	close(connfd);
 }
